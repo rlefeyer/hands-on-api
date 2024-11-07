8fa7f93d20809d60c60e2fadc639ed141aef45cb
Baptiste <batbox.longuepee@gmail.com>
2024-11-07 08:52:18 +0100

    docs(menu, orders, restaurants, users)

--- src/menus/menus.controller.ts
+++ src/menus/menus.controller.ts
@@ -2,32 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { MenusService } from './menus.service';
 import { CreateMenuDto } from './dto/create-menu.dto';
 import { UpdateMenuDto } from './dto/update-menu.dto';
+import {ApiOkResponse} from "@nestjs/swagger";
 
 @Controller('menus')
 export class MenusController {
   constructor(private readonly menusService: MenusService) {}
 
   @Post()
+  @ApiOkResponse({status: 201, description: 'Menu created successfully'})
   create(@Body() createMenuDto: CreateMenuDto) {
     return this.menusService.create(createMenuDto);
   }
 
   @Get()
+  @ApiOkResponse({description: 'All menus returned successfully'})
   findAll() {
     return this.menusService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({description: 'Menu returned successfully'})
   findOne(@Param('id') id: string) {
     return this.menusService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({description: 'Menu updated successfully'})
   update(@Param('id') id: string, @Body() updateMenuDto: UpdateMenuDto) {
     return this.menusService.update(+id, updateMenuDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({description: 'Menu deleted successfully'})
   remove(@Param('id') id: string) {
     return this.menusService.remove(+id);
   }
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -2,32 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import {ApiOkResponse} from "@nestjs/swagger";
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
+  @ApiOkResponse({status: 201, description: 'Order created successfully'})
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
 
   @Get()
+  @ApiOkResponse({description: 'All orders returned successfully'})
   findAll() {
     return this.ordersService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({description: 'Order returned successfully'})
   findOne(@Param('id') id: string) {
     return this.ordersService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({description: 'Order updated successfully'})
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.ordersService.update(+id, updateOrderDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({description: 'Order deleted successfully'})
   remove(@Param('id') id: string) {
     return this.ordersService.remove(+id);
   }
