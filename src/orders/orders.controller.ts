c83c8cc1245ec3105e1bacd62085a567616f7d32
Thibaut009 <gabet.thibaut@gmail.com>
2024-11-07 15:33:20 +0100

    Add deprecated

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import {ApiOkResponse} from "@nestjs/swagger";
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('orders')
 export class OrdersController {
@@ -22,6 +22,7 @@ export class OrdersController {
 
   @Get(':id/menus')
   @ApiOkResponse({description: 'Order returned successfully'})
+  @ApiOperation({deprecated: true })
   findOne(@Param('id') id: string) {
     return this.ordersService.findOne(+id);
   }
