5b50edffb907aa07a40dec12ab5e1626a43e1557
WazeAbi <alexdalle12@gmail.com>
2024-11-07 14:24:25 +0100

    feat/MenuDoc : Creation of Swagger Doc for Menus

--- src/menus/dto/create-menu.dto.ts
+++ src/menus/dto/create-menu.dto.ts
@@ -1,8 +1,16 @@
-import { Restaurant } from "src/restaurants/entities/restaurant.entity";
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateMenuDto {
+    @ApiProperty({ description: 'Name of the menu' })
     nom: string;
+
+    @ApiProperty({ description: 'Description of the menu', required: false })
     description?: string;
+
+    @ApiProperty({ description: 'Price of the menu' })
     prix: number;
+
+    @ApiProperty({ description: 'Associated restaurant', type: () => Restaurant })
     restaurant: Restaurant;
 }
--- src/menus/menus.controller.ts
+++ src/menus/menus.controller.ts
@@ -1,13 +1,19 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+    ApiBody,
+    ApiOperation,
+    ApiParam,
+    ApiResponse,
+    ApiTags,
+} from '@nestjs/swagger';
 import { CreateMenuDto } from './dto/create-menu.dto';
 import { UpdateMenuDto } from './dto/update-menu.dto';
 import { MenusService } from './menus.service';
@@ -15,30 +21,41 @@ import { MenusService } from './menus.service';
 @ApiTags('menus')
 @Controller('menus')
 export class MenusController {
-  constructor(private readonly menusService: MenusService) {}
+    constructor(private readonly menusService: MenusService) {}
 
-  @Post()
-  create(@Body() createMenuDto: CreateMenuDto) {
-    return this.menusService.create(createMenuDto);
-  }
+    @Post()
+    @ApiOperation({ summary: 'Create a new Menu', description: 'Creates a new Menu' })
+    @ApiBody({ type: CreateMenuDto })
+    create(@Body() createMenuDto: CreateMenuDto) {
+        return this.menusService.create(createMenuDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.menusService.findAll();
-  }
+    @Get()
+    @ApiOperation({ summary: 'Get all menus', description: 'Retrieves a list of all menus' })
+    @ApiResponse({ status: 200, description: 'List of menus retrieved successfully' })
+    findAll() {
+        return this.menusService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.menusService.findOne(id);
-  }
+    @Get(':id')
+    @ApiOperation({ summary: 'Get a specific menu', description: 'Retrieves details of a specific menu' })
+    @ApiParam({ name: 'id', description: 'Menu ID' })
+    findOne(@Param('id') id: string) {
+        return this.menusService.findOne(id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMenuDto: UpdateMenuDto) {
-    return this.menusService.update(id, updateMenuDto);
-  }
+    @Patch(':id')
+    @ApiOperation({ summary: 'Update a menu', description: 'Updates details of an existing menu' })
+    @ApiParam({ name: 'id', description: 'Menu ID' })
+    @ApiBody({ type: UpdateMenuDto })
+    update(@Param('id') id: string, @Body() updateMenuDto: UpdateMenuDto) {
+        return this.menusService.update(id, updateMenuDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.menusService.remove(id);
-  }
+    @Delete(':id')
+    @ApiOperation({ summary: 'Delete a menu', description: 'Deletes an existing menu' })
+    @ApiParam({ name: 'id', description: 'Menu ID' })
+    remove(@Param('id') id: string) {
+        return this.menusService.remove(id);
+    }
 }
