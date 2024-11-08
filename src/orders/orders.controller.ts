dc20949d45d16c39780a03447162013f08f5da6e
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-11-08 09:13:25 +0100

    :sparkles: feat(orders.controller): Adding new routes
    Adding v2 routes for delete, patch, post and get
    Adding deprecated old routes

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -13,6 +13,7 @@ export class OrdersController {
 
 
   @Post()
+  @ApiOperation({ deprecated: true })
   @ApiResponse({status: 201, description: 'The record has been successfully created.'})
     @ApiResponse({status: 403, description: 'Forbidden.'})
     @ApiResponse({status: 400, description: 'Bad Request.'})
@@ -20,9 +21,24 @@ export class OrdersController {
     return this.ordersService.create(createOrderDto);
   }
 
+  @Post("/v2")
+  @ApiResponse({status: 201, description: 'The record has been successfully created.'})
+  @ApiResponse({status: 403, description: 'Forbidden.'})
+  @ApiResponse({status: 400, description: 'Bad Request.'})
+  createV2(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto);
+  }
+
   @Get()
+  @ApiOperation({ deprecated: true })
   @ApiResponse({status: 200, description: 'The records has been successfully returned.', type: [Order]})
   findAll() {
+    return this.ordersService.findAll();
+  };
+
+  @Get("/v2")
+  @ApiResponse({status: 200, description: 'The records has been successfully returned.', type: [Order]})
+  findAllV2() {
     return this.ordersService.findAll();
   };
 
@@ -53,16 +69,32 @@ export class OrdersController {
   }
   
   @Patch(':id')
+  @ApiOperation({deprecated: true})
     @ApiResponse({status: 200, description: 'The record has been successfully updated.', type: Order})
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.ordersService.update(+id, updateOrderDto);
   }
 
+  @Patch('/v2/:id')
+  @ApiResponse({status: 200, description: 'The record has been successfully updated.', type: Order})
+  updateV2(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
+    return this.ordersService.update(+id, updateOrderDto);
+  }
+
   @Delete(':id')
+  @ApiOperation({deprecated: true})
     @ApiResponse({status: 200, description: 'The record has been successfully deleted.'})
     @ApiResponse({status: 403, description: 'Forbidden.'})
     @ApiResponse({status: 404, description: 'Not Found.'})
   remove(@Param('id') id: string) {
+    return this.ordersService.remove(+id);
+  }
+
+  @Delete('/v2/:id')
+  @ApiResponse({status: 200, description: 'The record has been successfully deleted.'})
+  @ApiResponse({status: 403, description: 'Forbidden.'})
+  @ApiResponse({status: 404, description: 'Not Found.'})
+  removeV2(@Param('id') id: string) {
     return this.ordersService.remove(+id);
   }
 }
