25533691b3678182ae5486c2d4ecf658593a44c9
edixoo <paul1pruvost@gmail.com>
2024-11-28 10:14:22 +0100

    feat(): add db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,19 @@ import { UserModule } from './user/user.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { MenusModule } from './menus/menus.module';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [OrdersModule, UserModule, RestaurantsModule, MenusModule, ItemsModule],
+  imports: [OrdersModule, UserModule, RestaurantsModule, MenusModule, ItemsModule,  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '22052003paul',
+    database: 'food-delivery',
+    synchronize: true,
+    logging: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,22 @@
-export class CreateItemDto {}
+import {ApiProperty} from "@nestjs/swagger";
+import {IsString} from "class-validator";
+import {Restaurant} from "../../restaurants/entities/restaurant.entity";
+
+export class CreateItemDto {
+
+    @ApiProperty({example: 'MaxiBestOf'})
+    @IsString()
+    name: string;
+
+    @ApiProperty({example: 'Le meilleur menu de chez McDo'})
+    @IsString()
+    description: string;
+
+    @ApiProperty({example: 10})
+    @IsString()
+    prix: number;
+
+    @ApiProperty({example: { name: 'McDo', description: 'Le meilleur fast-food', adresse: '5 rue de la paix', menu: [{id: '1', name: 'MaxiBestOf', description: 'Le meilleur menu de chez McDo', prix: 10, restaurant: 'McDo'}], note: 10, horaires: '10h-22h'}})
+    @IsString()
+    restaurant: Restaurant;
+}
