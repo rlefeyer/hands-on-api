b372e6234d277289897a757bf21b98b53ee7a328
Clément Berdin <clement.berdin@gmail.com>
2024-11-28 16:22:22 +0100

    fix

--- graph/src/user/entities/user.entity.ts
+++ graph/src/user/entities/user.entity.ts
@@ -1,17 +1,30 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+
 @Entity()
 @ObjectType()
 export class User {
-  /**   * this decorator will help to auto generate id for the table.   */ @Field(
-    () => Int,
-  )
+  @Field()
   @PrimaryGeneratedColumn()
-  id: number;
-  @Field() @Column({ type: 'varchar', length: 30 }) name: string;
-  @Field() @Column({ type: 'varchar', length: 15 }) username: string;
-  @Field() @Column({ type: 'varchar', length: 40 }) email: string;
-  @Field() @Column({ type: 'int' }) age: number;
-  @Field() @Column({ type: 'varchar' }) password: string;
-  @Field() @Column({ type: 'enum', enum: ['m', 'f', 'u'] }) gender: string;
+  id: string;
+
+  @Field()
+  @Column({ type: 'varchar', length: 60 })
+  name: string;
+
+  @Field()
+  username: string | null;
+
+  @Field()
+  @Column({ type: 'varchar', length: 255 })
+  email: string;
+
+  @Field()
+  age: number;
+
+  @Field()
+  gender: string;
+
+  @Field()
+  password: string;
 }
