345a47a821dd64b724ad39a68abb7632aba23854
Clément Berdin <clement.berdin@gmail.com>
2024-11-28 10:15:20 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,35 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { OrdersModule } from './orders/orders.module';
-import { MenusModule } from './menus/menus.module';
-import { RestaurantsModule } from './restaurants/restaurants.module';
-import {ItemsModule} from "./Items/items.module";
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { OrdersModule } from "./orders/orders.module";
+import { MenusModule } from "./menus/menus.module";
+import { RestaurantsModule } from "./restaurants/restaurants.module";
+import { ItemsModule } from "./Items/items.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [UsersModule, OrdersModule, MenusModule, RestaurantsModule, ItemsModule],
+  imports: [
+    UsersModule,
+    OrdersModule,
+    MenusModule,
+    RestaurantsModule,
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "deliveroodb-clement-28ce.f.aivencloud.com",
+      port: 14466,
+      username: "avnadmin",
+      password: "",
+      database: "defaultdb",
+      entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+      synchronize: true,
+      logging: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
