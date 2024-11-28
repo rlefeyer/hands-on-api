343bfc753bbc4a592aeb9fecf1b4c0a629070594
Clément Berdin <clement.berdin@gmail.com>
2024-11-28 10:47:30 +0100

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,44 +1,36 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [];
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto): Promise<User> {
-    const user = new User();
-    user.name = createUserDto.name;
-    user.username = createUserDto.username;
-    user.email = createUserDto.email;
-    user.age = createUserDto.age;
-    user.gender = createUserDto.gender;
-    user.password = createUserDto.password;
-    this.users.push(user);
-    return Promise.resolve(user);
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
   }
 
-  findAll() {
-    return this.users;
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
-  findOne(id: string) {
-    return this.users.find((user) => user.id === id);
+  findOne(id: string): Promise<User> {
+    return this.userRepository.findOne({where: {id}});
   }
 
-  update(id: string, updateUserDto: UpdateUserDto) {
-    const user: User = this.users.find((user) => user.id === id);
-    user.name = updateUserDto.name;
-    user.username = updateUserDto.username;
-    user.email = updateUserDto.email;
-    user.age = updateUserDto.age;
-    user.gender = updateUserDto.gender;
-    user.password = updateUserDto.password;
-    return user;
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    await this.userRepository.update(id, updateUserDto);
+    return this.userRepository.findOne({where: {id}});
   }
 
-  remove(id: string) {
-    this.users = this.users.filter((user) => user.id !== id);
-    return this.users;
+  async remove(id: string): Promise<void> {
+    await this.userRepository.delete(id);
   }
 }
