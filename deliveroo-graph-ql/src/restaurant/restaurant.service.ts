d8fbf2a39836939294aebf4f1f53d2484c496f6d
Louis F. <72155852+itsmelouis@users.noreply.github.com>
2024-11-28 16:33:19 +0100

    feat: ajout du service pour `Restaurant`

--- deliveroo-graph-ql/src/restaurant/restaurant.service.ts
+++ deliveroo-graph-ql/src/restaurant/restaurant.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { CreateRestaurantInput } from './dto/create-restaurant.input';
-import { UpdateRestaurantInput } from './dto/update-restaurant.input';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 @Injectable()
 export class RestaurantService {
-  create(createRestaurantInput: CreateRestaurantInput) {
-    return 'This action adds a new restaurant';
-  }
+  constructor(
+    @InjectRepository(Restaurant) private repo: Repository<Restaurant>,
+  ) {}
+  // create(createRestaurantInput: CreateRestaurantInput) {
+  //   return 'This action adds a new restaurant';
+  // }
 
   findAll() {
-    return `This action returns all restaurant`;
+    return this.repo.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} restaurant`;
+    return this.repo.findOne({ where: { id } });
   }
 
-  update(id: number, updateRestaurantInput: UpdateRestaurantInput) {
-    return `This action updates a #${id} restaurant`;
-  }
+  // update(id: number, updateRestaurantInput: UpdateRestaurantInput) {
+  //   return `This action updates a #${id} restaurant`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} restaurant`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} restaurant`;
+  // }
 }
