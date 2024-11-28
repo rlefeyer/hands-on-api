28ef624efeaff4b8ebac12b2a11256d59ebcf40e
Antoinebac435 <antoinebacquet62@gmail.com>
2024-11-28 12:09:44 +0100

    Fix ( User Entity ) : Delete phone

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -11,9 +11,6 @@ export class User {
   name: string;
   @ApiProperty({ example: "Michel78", required: false })
   username: string | null;
-  @Column({ type: "varchar", length: 30 })
-  @ApiProperty({ example: "+33 7 66 23 97 97", required: false })
-  phone: string | null;
   @Column({ type: "varchar", length: 255 })
   @ApiProperty({ example: "42 rue Sainte-Catherine", required: true })
   email: string;
