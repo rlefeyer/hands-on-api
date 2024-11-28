d08db09ae63da39a3bee598c974e6ba08cb218a5
Clément Berdin <clement.berdin@gmail.com>
2024-11-28 10:22:09 +0100

    Update user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,12 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
+import {Column, PrimaryGeneratedColumn} from "typeorm";
 
 export class User {
+  @PrimaryGeneratedColumn()
   @ApiProperty({ example: "1", required: true })
   id: string;
+  @Column({type: "varchar", length: 60})
   @ApiProperty({ example: "John Doe", required: true })
   name: string;
+  @Column({type: "varchar", length: 30})
   @ApiProperty({ example: "+33 7 66 23 97 97", required: false })
   phone: string | null;
+  @Column({type: "varchar", length: 255})
   @ApiProperty({ example: "42 rue Sainte-Catherine", required: true })
   address: string;
 }
