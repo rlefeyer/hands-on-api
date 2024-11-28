1731f9e6a1540f9ad41aa70b0facb6e2dd797a16
Baptiste <batbox.longuepee@gmail.com>
2024-11-28 13:50:20 +0100

    feat(create): added creation function for menus and orders

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Order} from "./entities/order.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class OrdersService {
+
+  constructor(
+    @InjectRepository(Order)
+    private readonly ordersRepository: Repository<Order>
+  ) {
+  }
+
   create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+    const name = createOrderDto.Name;
+    const menus = createOrderDto.Menus;
+    const price = createOrderDto.price;
+    const User = createOrderDto.User;
+    return this.ordersRepository.save({name, menus, price, User});
   }
 
   findAll() {
