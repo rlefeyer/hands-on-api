ee0a66bac95c85fb4b007e97f44438f4104d0c7f
distrib-ps <theodelhay@orange.fr>
2024-11-28 15:13:30 +0100

    feat: debut resolver

--- deliveroo-graph-ql/src/app.module.ts
+++ deliveroo-graph-ql/src/app.module.ts
@@ -1,9 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
