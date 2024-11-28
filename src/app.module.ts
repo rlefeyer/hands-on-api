2e0ea8a21081ac6666281173bf49ede409fb2f3b
Lucas Durbec <kd.daemon.kd@gmail.com>
2024-11-28 10:26:27 +0100

    Postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,30 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 import { MenusModule } from './menus/menus.module';
 import { OrdersModule } from './orders/orders.module';
 import { UsersModule } from './users/users.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
-  imports: [RestaurantsModule, MenusModule, OrdersModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    RestaurantsModule,
+    MenusModule,
+    OrdersModule,
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('HOST'),
+        port: +configService.getOrThrow('PORT'),
+        username: configService.getOrThrow('USERNAME'),
+        password: configService.getOrThrow('PASSWORD'),
+        database: configService.getOrThrow('DATABASE'),
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
