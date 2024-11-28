7d539488a41d7f16008f5b1ba98fb4ed9a7836fd
distrib-ps <theodelhay@orange.fr>
2024-11-28 15:52:39 +0100

    feat: pour le user ça fonctionne

--- deliveroo-graph-ql/src/user/entities/user.entity.ts
+++ deliveroo-graph-ql/src/user/entities/user.entity.ts
@@ -1,7 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 
+@Entity()
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column({ length: 100 })
+  name: string;
+
+  @Field()
+  @Column({ length: 255 })
+  address: string;
+
+  @Field()
+  @Column({ length: 15 })
+  phone: string;
+
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
 }
--- deliveroo-graph-ql/src/user/user.service.ts
+++ deliveroo-graph-ql/src/user/user.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  findAll(): Promise<User[]> {
+    return this.repo.find();
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  findOne(id: number): Promise<User> {
+    return this.repo.findOne({ where: { id } });
   }
 }
