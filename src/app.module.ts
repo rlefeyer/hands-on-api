4d08616ee29d7f0b9bd51f0fc0fb378e4cf216de
mindsek <contactmindsek@gmail.com>
2024-11-28 10:43:25 +0100

    feat: add config for module TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemsModule } from './items/items.module';
 import { MenusModule } from './menus/menus.module';
 import { OrdersModule } from './orders/orders.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
-import { ItemsModule } from './items/items.module';
+
+const ormconfig = {
+  type: 'postgres' as const,
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '4188',
+  entities: [User],
+  database: 'deliveroo',
+  synchronize: true,
+  logging: true,
+};
 
 @Module({
-  imports: [OrdersModule, RestaurantsModule, MenusModule, UsersModule, ItemsModule],
+  imports: [
+    OrdersModule,
+    RestaurantsModule,
+    MenusModule,
+    UsersModule,
+    ItemsModule,
+    TypeOrmModule.forRoot(ormconfig),
+  ],
   controllers: [],
   providers: [],
 })
