41f0502c13ca30059395b2cb2230ee54cb216170
Baptiste <batbox.longuepee@gmail.com>
2024-11-28 14:40:57 +0100

    Add nest graphql project

--- nest-orm/src/orders/versions/v2/orders.v2.modules.ts
+++ nest-orm/src/orders/versions/v2/orders.v2.modules.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from '../../orders.service';
 import {OrdersV2Controller} from "./orders.v2.controller";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Order} from "../../entities/order.entity";
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Order])],
     controllers: [OrdersV2Controller],
     providers: [OrdersService],
 })
