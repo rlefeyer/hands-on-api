9f0b3f5a6d35cccc0328d849db4b80ec842a3838
mindsek <contactmindsek@gmail.com>
2024-11-28 13:49:25 +0100

    refactor: menu

--- src/menus/dto/create-menu.dto.ts
+++ src/menus/dto/create-menu.dto.ts
@@ -2,15 +2,15 @@ import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateMenuDto {
-    @ApiProperty({ description: 'Name of the menu' })
-    nom: string;
+  @ApiProperty({ description: 'Name of the menu' })
+  name: string;
 
-    @ApiProperty({ description: 'Description of the menu', required: false })
-    description?: string;
+  @ApiProperty({ description: 'Description of the menu', required: false })
+  description?: string;
 
-    @ApiProperty({ description: 'Price of the menu' })
-    prix: number;
+  @ApiProperty({ description: 'Price of the menu' })
+  prix: number;
 
-    @ApiProperty({ description: 'Associated restaurant', type: () => Restaurant })
-    restaurant: Restaurant;
+  @ApiProperty({ description: 'Associated restaurant', type: () => Restaurant })
+  restaurant: Restaurant;
 }
--- src/menus/entities/menu.entity.ts
+++ src/menus/entities/menu.entity.ts
@@ -1,9 +1,20 @@
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class Menu {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
-  nom: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
   description?: string;
+
+  @Column()
   prix: number;
+
+  @ManyToOne(() => Restaurant, (restaurant) => restaurant.items)
   restaurant: Restaurant;
 }
