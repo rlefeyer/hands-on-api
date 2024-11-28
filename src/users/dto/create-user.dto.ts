9976056da1f88ad168bf4452738ad5508092c17a
mindsek <contactmindsek@gmail.com>
2024-11-28 10:43:57 +0100

    refactor: update dto user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,34 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsMobilePhone,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+  @MinLength(3, { message: 'Name must be at least 3 characters long' })
+  @MaxLength(30, { message: 'Name must be at most 30 characters long' })
+  @Matches(/^[a-zA-Z0-9\s]*$/, {
+    message: 'Name must contain only letters, numbers and spaces',
+  })
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @MinLength(3, { message: 'Address must be at least 3 characters long' })
+  @MaxLength(100, { message: 'Address must be at most 100 characters long' })
   address: string;
 
   @IsString()
   @IsNotEmpty()
+  @MinLength(10, {
+    message: 'Phone number must be at least 10 characters long',
+  })
+  @MaxLength(15, { message: 'Phone number must be at most 15 characters long' })
+  @IsMobilePhone('fr-FR')
   phone: string;
 }
