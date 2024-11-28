edb029e170fbc3c0df22979356eb30a1a0c01a3e
mindsek <contactmindsek@gmail.com>
2024-11-28 10:43:46 +0100

    refactor: update entity user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ length: 30 })
   name: string;
+
+  @Column({ length: 100 })
   address: string;
+
+  @Column({ length: 15 })
   phone: string;
 }
