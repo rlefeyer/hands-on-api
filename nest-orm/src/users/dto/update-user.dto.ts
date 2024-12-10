e5004e826e95e2f5e0a7a4c6350bf245ebc05a73
Baptiste <34516070+BaptisteLonguepee@users.noreply.github.com>
2024-12-10 13:57:50 +0100

    Debug eslint, fix entity and dto

--- nest-orm/src/users/dto/update-user.dto.ts
+++ nest-orm/src/users/dto/update-user.dto.ts
@@ -1,14 +1,35 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsInt, IsNotEmpty, IsString, Min } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @ApiPropertyOptional({ description: 'The name of the user (optional)' })
-  Name?: string;
+  @ApiProperty({ description: 'The name of the user' })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 
-  @ApiPropertyOptional({ description: 'The address of the user (optional)' })
-  Address?: string;
+  @ApiProperty({ description: 'The username of the user' })
+  @IsString()
+  @IsNotEmpty()
+  username: string;
 
-  @ApiPropertyOptional({ description: 'The phone number of the user (optional)' })
-  Phone?: string;
+  @ApiProperty({ description: 'The email address of the user' })
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({ description: 'The age of the user', example: 25 })
+  @IsInt()
+  @Min(0, { message: 'Age must be a positive number.' })
+  age: number;
+
+  @ApiProperty({ description: 'The password of the user' })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @ApiProperty({ description: 'The gender of the user', enum: ['m', 'f', 'u'] })
+  @IsEnum(['m', 'f', 'u'], { message: 'Gender must be "m", "f", or "u".' })
+  gender: string;
 }
--- nest-orm/src/users/entities/user.entity.ts
+++ nest-orm/src/users/entities/user.entity.ts
@@ -5,19 +5,19 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 50 }) // Déclare une colonne dans la base
+  @Column({ type: 'varchar', length: 50 })
   name: string;
 
-  @Column({ type: 'varchar', length: 30, unique: true }) // Colonne avec contrainte d'unicité
+  @Column({ type: 'varchar', length: 30, unique: true })
   username: string;
 
-  @Column({ type: 'varchar', length: 100, unique: true }) // Email doit être unique
+  @Column({ type: 'varchar', length: 100, unique: true })
   email: string;
 
-  @Column({ type: 'int' }) // Colonne de type entier
+  @Column({ type: 'int' })
   age: number;
 
-  @Column({ type: 'varchar', length: 255 }) // Mot de passe hashé
+  @Column({ type: 'varchar', length: 255 })
   password: string;
 
   @Column({ type: 'enum', enum: ['m', 'f', 'u'] })
