7430845ba259b88be5022c04e340ed22ba7535db
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-12-10 15:45:13 +0100

    :white_check_mark: test(items.service.spec.ts): Update item service test

--- rest/src/items/items.service.spec.ts
+++ rest/src/items/items.service.spec.ts
@@ -1,4 +1,3 @@
-import {OrdersService} from "../orders/orders.service";
 import {ItemsService} from "./items.service";
 import {Test} from "@nestjs/testing";
 
@@ -31,19 +30,15 @@ describe("ItemsService", () => {
         itemsService = module.get<ItemsService>(ItemsService);
     });
 
-    describe("findAll", () => {
-        it("should return an array of items", async () => {
+    it("should return an array of items", async () => {
+        jest.spyOn(itemsService, "findAll").mockResolvedValue([item as any]);
 
-            jest.spyOn(itemsService, "findAll").mockResolvedValue([item as any]);
+        const items = await itemsService.findAll();
 
-            const items = await itemsService.findAll();
-
-            expect(items).toEqual([item]);
-        });
-
+        expect(items).toEqual([item]);
     });
 
-    it("should return a item", async () => {
+    it("should return an item", async () => {
         const mockItemRepository = {
             findOne: jest.fn().mockResolvedValue({
                 "id": 2,
@@ -66,8 +61,7 @@ describe("ItemsService", () => {
         expect(mockItemRepository.findOne).toHaveBeenCalledWith({where: {id: 1}});
     });
 
-    it("should create a item", async () => {
-
+    it("should create an item", async () => {
         jest.spyOn(itemsService, "create").mockResolvedValue(item as any);
 
         const newItem = await itemsService.create(item as any);
@@ -76,62 +70,21 @@ describe("ItemsService", () => {
         expect(itemsService.create).toHaveBeenCalledWith(item);
     });
 
-    it("should update a item", async () => {
-        const mockItemRepository = {
-            update: jest.fn().mockResolvedValue({affected: 1}),
-            findOne: jest.fn().mockResolvedValue({
-                "id": 2,
-                "name": "MaxiBestOf",
-                "description": "Le meilleur menu de chez McDo",
-                "prix": 10,
-            }),
-        };
+    it("should update an item", async () => {
+        jest.spyOn(itemsService, "update").mockResolvedValue(item as any);
 
-        const itemsService = new ItemsService(mockItemRepository as any);
+        const updatedItem = await itemsService.update(1, item as any);
 
-        const updateItemDto = {
-            "id": 2,
-            "name": "MaxiBestOf",
-            "description": "Le meilleur menu de chez McDo",
-            "prix": 10,
-        };
-
-        const updatedItem = await itemsService.update(1, updateItemDto as any);
-
-        expect(updatedItem).toEqual({
-            "id": 2,
-            "name": "MaxiBestOf",
-            "description": "Le meilleur menu de chez McDo",
-            "prix": 10,
-        });
-
-        expect(mockItemRepository.update).toHaveBeenCalledWith(1, expect.objectContaining(updateItemDto));
-        expect(mockItemRepository.findOne).toHaveBeenCalledWith({where: {id: 1}});
+        expect(updatedItem).toEqual(item);
+        expect(itemsService.update).toHaveBeenCalledWith(1, expect.objectContaining(item));
     });
 
-    it("should remove a item", async () => {
-        const mockItemRepository = {
-            delete: jest.fn().mockResolvedValue({affected: 1}),
-        };
-
-        const itemsService = new ItemsService(mockItemRepository as any);
+    it("should remove an item", async () => {
+        jest.spyOn(itemsService, "remove").mockResolvedValue(true);
 
         const result = await itemsService.remove(1);
 
         expect(result).toBe(true);
-        expect(mockItemRepository.delete).toHaveBeenCalledWith(1);
-    });
-
-    it("should return false if item is not deleted", async () => {
-        const mockItemsRepository = {
-            delete: jest.fn().mockResolvedValue({affected: 0}),
-        };
-
-        const userService = new ItemsService(mockItemsRepository as any);
-
-        const result = await userService.remove(1);
-
-        expect(result).toBe(false);
-        expect(mockItemsRepository.delete).toHaveBeenCalledWith(1);
+        expect(itemsService.remove).toHaveBeenCalledWith(1);
     });
 });
