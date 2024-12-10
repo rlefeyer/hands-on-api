7f24e74438946e44cc96fb56eae8ba9cc0788bd1
Louis F. <72155852+itsmelouis@users.noreply.github.com>
2024-12-10 15:07:35 +0100

    test: pass all test

--- bowling/src/bowling-score.ts
+++ bowling/src/bowling-score.ts
@@ -6,6 +6,29 @@ export class BowlingScore {
   }
 
   calculate(): number {
-    return this.frames.reduce((a, b) => a + b, 0);
+    let totalScore = 0;
+    let frameIndex = 0;
+
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.frames[frameIndex] === 10) {
+        totalScore +=
+          10 +
+          (this.frames[frameIndex + 1] || 0) +
+          (this.frames[frameIndex + 2] || 0);
+        frameIndex += 1;
+      } else if (
+        (this.frames[frameIndex] || 0) + (this.frames[frameIndex + 1] || 0) ===
+        10
+      ) {
+        totalScore += 10 + (this.frames[frameIndex + 2] || 0);
+        frameIndex += 2;
+      } else {
+        totalScore +=
+          (this.frames[frameIndex] || 0) + (this.frames[frameIndex + 1] || 0);
+        frameIndex += 2;
+      }
+    }
+
+    return totalScore;
   }
 }
