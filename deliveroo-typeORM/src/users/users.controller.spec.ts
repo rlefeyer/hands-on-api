7ea1828dfcce458c5a1c7d1f4ad8f4e4aea0e7ee
distrib-ps <theodelhay@orange.fr>
2024-12-10 11:31:27 +0100

    feat: test unitaire users

--- deliveroo-typeORM/src/users/users.controller.spec.ts
+++ deliveroo-typeORM/src/users/users.controller.spec.ts
@@ -1,20 +1,121 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
+
+  const mockUserRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+    findOneBy: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
+  const mockUser: User = {
+    id: 1,
+    name: 'John Doe',
+    address: '123 Rue de Paris',
+    phone: '0666666666',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new user', async () => {
+      jest.spyOn(service, 'create').mockResolvedValue(mockUser);
+
+      const createUserDto = {
+        name: 'John Doe',
+        address: '123 Rue de Paris',
+        phone: '0666666666',
+      };
+
+      expect(await controller.create(createUserDto)).toEqual(mockUser);
+      expect(service.create).toHaveBeenCalledWith(createUserDto);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      jest.spyOn(service, 'findAll').mockResolvedValue([mockUser]);
+
+      expect(await controller.findAll()).toEqual([mockUser]);
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a user by ID', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockUser);
+
+      expect(await controller.findOne('1')).toEqual(mockUser);
+      expect(service.findOne).toHaveBeenCalledWith(1);
+    });
+
+    it('should throw an error if user is not found', async () => {
+      jest.spyOn(service, 'findOne').mockRejectedValue(
+        new Error('Utilisateur avec l\'ID 1 non trouvé.'),
+      );
+
+      await expect(controller.findOne('1')).rejects.toThrow(
+        'Utilisateur avec l\'ID 1 non trouvé.',
+      );
+      expect(service.findOne).toHaveBeenCalledWith(1);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user', async () => {
+      const updateUserDto = {
+        name: 'Jane Doe',
+        address: '456 Rue de Lyon',
+        phone: '0777777777',
+      };
+
+      jest.spyOn(service, 'update').mockResolvedValue({
+        ...mockUser,
+        ...updateUserDto,
+      });
+
+      expect(await controller.update('1', updateUserDto)).toEqual({
+        ...mockUser,
+        ...updateUserDto,
+      });
+      expect(service.update).toHaveBeenCalledWith(1, updateUserDto);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a user by ID', async () => {
+      jest.spyOn(service, 'remove').mockResolvedValue(undefined);
+
+      expect(await controller.remove('1')).toBeUndefined();
+      expect(service.remove).toHaveBeenCalledWith(1);
+    });
   });
 });
--- deliveroo-typeORM/src/users/users.service.spec.ts
+++ deliveroo-typeORM/src/users/users.service.spec.ts
@@ -1,18 +1,127 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repository: Repository<User>;
+
+  const mockUserRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+    findOneBy: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
+  const mockUser: User = {
+    id: 1,
+    name: 'John Doe',
+    address: '123 Rue de Paris',
+    phone: '0666666666',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create and save a new user', async () => {
+      jest.spyOn(repository, 'create').mockReturnValue(mockUser);
+      jest.spyOn(repository, 'save').mockResolvedValue(mockUser);
+
+      const createUserDto = {
+        name: 'John Doe',
+        address: '123 Rue de Paris',
+        phone: '0666666666',
+      };
+
+      expect(await service.create(createUserDto)).toEqual(mockUser);
+      expect(repository.create).toHaveBeenCalledWith(createUserDto);
+      expect(repository.save).toHaveBeenCalledWith(mockUser);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      jest.spyOn(repository, 'find').mockResolvedValue([mockUser]);
+
+      expect(await service.findAll()).toEqual([mockUser]);
+      expect(repository.find).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a user by ID', async () => {
+      jest.spyOn(repository, 'findOneBy').mockResolvedValue(mockUser);
+
+      expect(await service.findOne(1)).toEqual(mockUser);
+      expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    });
+
+    it('should throw NotFoundException if user is not found', async () => {
+      jest.spyOn(repository, 'findOneBy').mockResolvedValue(null);
+
+      await expect(service.findOne(1)).rejects.toThrow(
+        new NotFoundException(`Utilisateur avec l'ID 1 non trouvé.`),
+      );
+      expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    });
+  });
+
+  describe('update', () => {
+    it('should update and return the updated user', async () => {
+      const updateUserDto = {
+        name: 'Jane Doe',
+        address: '456 Rue de Lyon',
+        phone: '0777777777',
+      };
+
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockUser);
+      jest.spyOn(repository, 'update').mockResolvedValue(undefined);
+      jest.spyOn(repository, 'findOneBy').mockResolvedValue({
+        ...mockUser,
+        ...updateUserDto,
+      });
+
+      expect(await service.update(1, updateUserDto)).toEqual({
+        ...mockUser,
+        ...updateUserDto,
+      });
+      expect(repository.update).toHaveBeenCalledWith(1, {
+        ...mockUser,
+        ...updateUserDto,
+      });
+      expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a user by ID', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockUser);
+      jest.spyOn(repository, 'remove').mockResolvedValue(undefined);
+
+      expect(await service.remove(1)).toBeUndefined();
+      expect(repository.remove).toHaveBeenCalledWith(mockUser);
+    });
   });
 });
