7423cc8f8c93eac5339291b48cd2e3a2dd1c68ca
Baptiste <batbox.longuepee@gmail.com>
2024-12-12 11:39:24 +0100

    Update auth.controller.ts

--- nest-orm/src/auth/auth.controller.ts
+++ nest-orm/src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import {Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards} from '@nestjs/common';
+import {Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards, Request} from '@nestjs/common';
 import {AuthService} from "./auth.service";
 import {AuthGuard} from "./auth.guard";
 
 @Controller('auth')
 export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
     @HttpCode(HttpStatus.OK)
     @Post('login')
     signIn(@Body() signInDto: Record<string, any>) {
-        return AuthService.signIn(signInDto.username, signInDto.password);
+        return this.authService.signIn(signInDto.username, signInDto.password);
     }
 
     @UseGuards(AuthGuard)
