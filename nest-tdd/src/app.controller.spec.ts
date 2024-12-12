2745f114a45227f0c4fca93479cea6127da54e01
thibaut009 <gabet.thibaut@gmail.com>
2024-12-12 10:31:36 +0100

    update

--- nest-tdd/src/app.controller.spec.ts
+++ nest-tdd/src/app.controller.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {empty} from "rxjs";
 
 describe('AppController', () => {
   let appController: AppController;
