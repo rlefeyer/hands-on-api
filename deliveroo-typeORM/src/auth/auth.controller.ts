f5eb5f2dd12407cd25d9d30399478facbe896e4e
Louis F. <72155852+itsmelouis@users.noreply.github.com>
2024-12-12 11:30:03 +0100

    feat: ajout du endpoint login

--- deliveroo-typeORM/src/auth/auth.controller.ts
+++ deliveroo-typeORM/src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+  ){}
+
+  @Post('login')
+  @ApiOperation({ summary: 'Se connecter' })
+  @ApiResponse({
+    status: 200,
+    description: 'Connecté avec succès.',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Identifiants incorrects',
+  })
+  async login(username: string, password: string) {
+    return this.authService.signIn(username, password);
+  }
+}
