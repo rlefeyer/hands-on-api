dd0ae5954915ed029de0f86f8873ffb45a2c1b55
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-12-13 09:02:55 +0100

    Bowling

---
+++ bowling/src/app.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export class AppService {
+    private total = 0;
+
+    constructor() {
+    }
+
+    getHello(): string {
+        return "Hello World!";
+    }
+
+    getTotalScore(data: Array<number>): number {
+        let previousValue = 0;
+        data.map((score, index) => {
+            this.total += score;
+
+            if (index % 2 === 0)
+                if (previousValue + score === 10) this.getSpare(data, index);
+            
+            previousValue = score;
+        });
+        return this.total;
+    }
+
+    private getSpare(arrayValue: Array<number>, indexScore: number) {
+        this.total += arrayValue[indexScore + 1];
+    }
+
+    private getStrike() {
+    }
+}
